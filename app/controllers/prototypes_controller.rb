3f031176c4e42bee021c1f99d400c033dcfedbec
Koki-Nambu <ztailang414@gmail.com>
2022-01-05 18:20:35 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,66 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :new, :destroy]
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.create(prototype_params)
+    @prototype.save
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    if user_signed_in?
+      if current_user.id == @prototype.user.id
+        redirect_to action: :edit
+      else
+        redirect_to action: :index
+      end
+    end
+  end
+end
